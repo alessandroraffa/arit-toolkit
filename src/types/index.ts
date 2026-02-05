1596b3b00fb571628981eb43690fcc0ed25f95d0
Alessandro Raffa <contact@alessandroraffa.eu>
2026-02-05 20:28:45 +0100

    refactor(types): remove unused feature interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,3 @@
 export type TimestampFormat = 'YYYYMMDDHHmm' | 'YYYYMMDD' | 'YYYYMMDDHHmmss' | 'ISO';
 
 export type LogLevel = 'off' | 'error' | 'warn' | 'info' | 'debug';
-
-export interface Feature {
-  readonly id: string;
-  readonly commandId: string;
-  register(): void;
-}
